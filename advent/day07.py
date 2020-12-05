c6c50271d626c6f111b7de5b65291cec538e876d
Kevin Wilson <khwilson@gmail.com>
2020-12-05 00:13:23 -0500

    Precommit some later date shells

---
+++ advent/day07.py
@@ -0,0 +1,20 @@
+""" AOC Day 7 """
+import itertools as its
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+
+
+def first(filename: Union[str, Path]) -> int:
+    """
+    Part 1
+    """
+    return None
+
+
+def second(filename: Union[str, Path]) -> int:
+    """
+    Part 2
+    """
+    return None
---
+++ tests/test_day06.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from advent import day06
+
+# def test_first(fixture_path: Path):
+#     assert day06.first(fixture_path / "example06.txt") == 514579
+
+
+# def test_second(fixture_path: Path):
+#     assert day06.second(fixture_path / "example06.txt") == 241861950
