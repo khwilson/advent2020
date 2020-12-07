5714fce125dffb835157794e1275e099efcf7bd3
Kevin Wilson <khwilson@gmail.com>
2020-12-07 08:30:26 -0500

    Day 6

--- advent/day06.py
+++ advent/day06.py
@@ -1,20 +1,34 @@
 """ AOC Day 6 """
-import itertools as its
+import re
+from collections import Counter
 from pathlib import Path
 from typing import Union
-
-import numpy as np
 
 
 def first(filename: Union[str, Path]) -> int:
     """
-    Part 1
+    In each group of letters (separated by a double newline), return
+    the number of distinct letters.
     """
-    return None
+    with open(filename, "rt") as infile:
+        groups = infile.read().strip().split("\n\n")
+
+    return sum(len(set(re.sub(r"\s", "", group))) for group in groups)
 
 
 def second(filename: Union[str, Path]) -> int:
     """
-    Part 2
+    For each group of letters (separated by a double newline), count the
+    number of letters that appear on all the lines in the group. Then
+    return the total number of such letters. (Letters do not need to
+    be distinct _between_ groups, only within groups.)
     """
-    return None
+    with open(filename, "rt") as infile:
+        groups = infile.read().strip().split("\n\n")
+
+    groups = [group.split("\n") for group in groups]
+
+    return sum(
+        sum(val == len(group) for val in Counter("".join(group)).values())
+        for group in groups
+    )
--- tests/test_day06.py
+++ tests/test_day06.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 from advent import day06
 
-# def test_first(fixture_path: Path):
-#     assert day06.first(fixture_path / "example06.txt") == 514579
+
+def test_first(fixture_path: Path):
+    assert day06.first(fixture_path / "example06.txt") == 11
 
 
-# def test_second(fixture_path: Path):
-#     assert day06.second(fixture_path / "example06.txt") == 241861950
+def test_second(fixture_path: Path):
+    assert day06.second(fixture_path / "example06.txt") == 6
