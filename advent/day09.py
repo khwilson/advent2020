a72a27ad89aedc080f98e4c190be1ac832ad43b9
Kevin Wilson <khwilson@gmail.com>
2020-12-09 00:16:24 -0500

    Day 9

--- advent/day09.py
+++ advent/day09.py
@@ -3,18 +3,35 @@ import itertools as its
 from pathlib import Path
 from typing import Union
 
-import numpy as np
+
+def first(filename: Union[str, Path], preamble_length: int = 25) -> int:
+    """
+    Return the first number that cannot be written as the sum of two numbers
+    among the previous `preamble_length` numbers
+    """
+    with open(filename, "rt") as infile:
+        data = [int(line.strip()) for line in infile]
+
+    for i, num in enumerate(data[preamble_length:], preamble_length):
+        for left, right in its.combinations(data[i - preamble_length : i], 2):
+            if num == left + right:
+                break
+        else:
+            return num
 
 
-def first(filename: Union[str, Path]) -> int:
+def second(filename: Union[str, Path], preamble_length: int = 25) -> int:
     """
-    Part 1
+    Find the first contiguous set of numbers that sum to the return value of
+    `first`. Then return the sum of the minimum and maximum of these numbers
     """
-    return None
+    num = first(filename, preamble_length=preamble_length)
+    with open(filename, "rt") as infile:
+        data = [int(line.strip()) for line in infile]
 
-
-def second(filename: Union[str, Path]) -> int:
-    """
-    Part 2
-    """
-    return None
+    for left in range(len(data)):
+        total = 0
+        for right, val in enumerate(data[left:], left):
+            total += val
+            if total == num:
+                return min(data[left : right + 1]) + max(data[left : right + 1])
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from advent import day05
-
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
+from advent import day09
 
 
-# def test_second(fixture_path: Path):
-#     assert day05.second(fixture_path / "example05.txt") == 241861950
+def test_first(fixture_path: Path):
+    assert day09.first(fixture_path / "example09.txt", preamble_length=5) == 127
+
+
+def test_second(fixture_path: Path):
+    assert day09.second(fixture_path / "example09.txt", preamble_length=5) == 62
