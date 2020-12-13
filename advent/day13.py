d09a758b9e06c96d6482ea9d0b2aefbe7b1816d7
Kevin Wilson <khwilson@gmail.com>
2020-12-13 00:31:45 -0500

    Day 13

--- advent/day13.py
+++ advent/day13.py
@@ -1,20 +1,55 @@
-""" AOC Day 8 """
-import itertools as its
+""" AOC Day 13 """
 from pathlib import Path
 from typing import Union
-
-import numpy as np
 
 
 def first(filename: Union[str, Path]) -> int:
     """
-    Part 1
+    For each bus_id, look for the smallest multiple of bus_id greater than
+    earliest_timestamp.
+
+    Among all these multiples, let M be the closest to earliest_timestamp
+    belonging to bus_id. Return (M - earliest_timestamp) * bus_id
     """
-    return None
+    with open(filename, "rt") as infile:
+        earliest_timestamp = int(next(infile).strip())
+        buses_in_services = [
+            int(x) for x in next(infile).strip().split(",") if x != "x"
+        ]
+
+    # Just make sure that there a no buses with a 0 minute wait time
+    for bus_id in buses_in_services:
+        if earliest_timestamp % bus_id == 0:
+            return 0
+
+    # Else, the wait time is bus_id - (earliest_timestamp % bus_id)
+    wait_time, bus_id = min(
+        (bus_id - (earliest_timestamp % bus_id), bus_id) for bus_id in buses_in_services
+    )
+    return wait_time * bus_id
 
 
 def second(filename: Union[str, Path]) -> int:
     """
-    Part 2
+    Consider the collection of integers n_i at position r_i in the input
+    (0-delimited). Then solve the system of congruences x ≡ -r_i (mod n_i)
     """
-    return None
+    with open(filename, "rt") as infile:
+        next(infile)
+        buses_in_services = [
+            (int(x) - (mod % int(x)), int(x))
+            for mod, x in enumerate(next(infile).strip().split(","))
+            if x != "x"
+        ]
+
+    # Perform the Chinese Remainder Theorem. Note that this works because everything is prime
+    cur_val, cur_mod = buses_in_services[0]
+    for next_rem, next_mod in buses_in_services[1:]:
+        val = cur_val
+        while val % next_mod != next_rem:
+            val += cur_mod
+
+        cur_val = val
+        cur_mod *= next_mod
+
+    return cur_val
--- tests/test_day13.py
+++ tests/test_day13.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from advent import day05
-
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
+from advent import day13
 
 
-# def test_second(fixture_path: Path):
-#     assert day05.second(fixture_path / "example05.txt") == 241861950
+def test_first(fixture_path: Path):
+    assert day13.first(fixture_path / "example13.txt") == 295
+
+
+def test_second(fixture_path: Path):
+    assert day13.second(fixture_path / "example13.txt") == 1068781
