6601759117ef0d445152ce9ad82d6f05488a7331
Kevin Wilson <khwilson@gmail.com>
2020-12-15 00:48:32 -0500

    Day 15

--- advent/day15.py
+++ advent/day15.py
@@ -1,20 +1,47 @@
-""" AOC Day 8 """
-import itertools as its
+""" AOC Day 15 """
 from pathlib import Path
 from typing import Union
 
-import numpy as np
+
+def play_game(filename: Union[str, Path], num_rounds: int) -> int:
+    """ See instructions on `first` """
+    last_spoken = {}
+    with open(filename, "rt") as infile:
+        data = [int(x) for x in infile.read().strip().split(",")]
+
+    for i, datum in enumerate(data[:-1], 1):
+        last_spoken[datum] = i
+
+    next_up = data[-1]
+    for turn in range(len(last_spoken) + 1, 2020):
+        if next_up not in last_spoken:
+            last_spoken[next_up] = turn
+            next_up = 0
+        else:
+            next_next_up = turn - last_spoken[next_up]
+            last_spoken[next_up] = turn
+            next_up = next_next_up
+
+    return next_up
 
 
 def first(filename: Union[str, Path]) -> int:
     """
-    Part 1
+    Play a game where, after a starting sequence (the contents of filename),
+    at each round the next value is:
+        * 0 if the previous value had never been seen before
+        * If the previous value had been seen in a previous round, the number
+          of rounds between sightings
+
+    Return the 2020th number to show up (inclusive of the starting sequence)
     """
-    return None
+    return play_game(filename, 2020)
 
 
 def second(filename: Union[str, Path]) -> int:
     """
-    Part 2
+    Same as Part 1, but just go for 30m rounds. There's almost certainly
+    a clever way to do this, but just running the program for 30m rounds
+    doesn't actually take that long
     """
-    return None
+    return play_game(filename, 30_000_000)
--- tests/test_day15.py
+++ tests/test_day15.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
-from advent import day05
+from advent import day15
 
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
+
+def test_first(fixture_path: Path):
+    assert day15.first(fixture_path / "example15.txt") == 1
+    assert day15.first(fixture_path / "example153.txt") == 10
 
 
 # def test_second(fixture_path: Path):
-#     assert day05.second(fixture_path / "example05.txt") == 241861950
+#     assert day15.second(fixture_path / "example15.txt") == 241861950
