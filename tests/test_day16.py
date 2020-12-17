7b1b5a65e7467e307692bfce43abfede7cf357a8
Kevin Wilson <khwilson@gmail.com>
2020-12-17 10:44:38 -0500

    Day 16: Add test file

--- tests/test_day16.py
+++ tests/test_day16.py
@@ -1,10 +1,6 @@
 from pathlib import Path
 
-from advent import day05
+from advent import day16
 
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
-
-
-# def test_second(fixture_path: Path):
-#     assert day05.second(fixture_path / "example05.txt") == 241861950
+def test_first(fixture_path: Path):
+    assert day16.first(fixture_path / "example16.txt") == 71
