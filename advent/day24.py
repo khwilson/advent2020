5c6d15b8e11f6ded604375b10bb0cda0c00db692
Kevin Wilson <khwilson@gmail.com>
2020-12-24 11:08:57 -0500

    Day 24

--- advent/day24.py
+++ advent/day24.py
@@ -1,20 +1,98 @@
 """ AOC Day 8 """
-import itertools as its
+from collections import defaultdict
 from pathlib import Path
-from typing import Union
+from typing import Dict, List, Tuple, Union
 
-import numpy as np
+WHITE = 0
+BLACK = 1
+
+
+def get_board(filename: Union[str, Path]) -> Dict[Tuple[int, int, int], int]:
+    """
+    Parse the incoming file for the starting state of the hexagonal Life board
+    """
+    tiles = defaultdict(lambda: WHITE)
+    with open(filename, "rt") as infile:
+        for line in infile:
+            a, r, c = 0, 0, 0
+            line = line.strip()
+            idx = 0
+            while idx < len(line):
+                direction = line[idx]
+                if direction == "n":
+                    direction = direction + line[idx + 1]
+                    if direction == "nw":
+                        a, r, c = 1 - a, r - (1 - a), c - (1 - a)
+                    elif direction == "ne":
+                        a, r, c = 1 - a, r - (1 - a), c + a
+                    else:
+                        raise ValueError
+                    idx += 2
+                elif direction == "s":
+                    direction = direction + line[idx + 1]
+                    if direction == "sw":
+                        a, r, c = 1 - a, r + a, c - (1 - a)
+                    elif direction == "se":
+                        a, r, c = 1 - a, r + a, c + a
+                    else:
+                        raise ValueError
+                    idx += 2
+                elif direction == "e":
+                    a, r, c = a, r, c + 1
+                    idx += 1
+                elif direction == "w":
+                    a, r, c = a, r, c - 1
+                    idx += 1
+                else:
+                    raise ValueError(f"Bad direction: {direction}")
+            tiles[(a, r, c)] = WHITE if tiles[(a, r, c)] == BLACK else BLACK
+    return tiles
+
+
+def neighbors(a: int, r: int, c: int) -> List[Tuple[int, int, int]]:
+    """
+    Return the list of all six neighbors of the passed coordinate
+    """
+    return (
+        (1 - a, r - (1 - a), c - (1 - a)),  # NW
+        (1 - a, r - (1 - a), c + a),  # NE
+        (a, r, c + 1),  # E
+        (1 - a, r + a, c + a),  # SE
+        (1 - a, r + a, c - (1 - a)),  # SW
+        (a, r, c - 1),  # W
+    )
 
 
 def first(filename: Union[str, Path]) -> int:
     """
-    Part 1
+    How many black square are there on the board?
     """
-    return None
+    return sum(get_board(filename).values())
 
 
 def second(filename: Union[str, Path]) -> int:
     """
-    Part 2
+    Play a game of life for 100 rounds. After which, how many black
+    squares are on the board?
     """
-    return None
+    board = get_board(filename)
+
+    for _ in range(100):
+        new_board = defaultdict(lambda: WHITE)
+        to_visit = {key for key, val in board.items() if val == BLACK}
+        to_visit = to_visit | {
+            neighbor for tile in to_visit for neighbor in neighbors(*tile)
+        }
+        for tile in to_visit:
+            count_black_neighbors = sum(
+                board[neighbor] == BLACK for neighbor in neighbors(*tile)
+            )
+            if board[tile] == WHITE:
+                if count_black_neighbors == 2:
+                    new_board[tile] = BLACK
+            else:
+                if count_black_neighbors == 1 or count_black_neighbors == 2:
+                    new_board[tile] = BLACK
+        board = new_board
+
+    return sum(board.values())
--- tests/test_day24.py
+++ tests/test_day24.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from advent import day05
-
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
+from advent import day24
 
 
-# def test_second(fixture_path: Path):
-#     assert day05.second(fixture_path / "example05.txt") == 241861950
+def test_first(fixture_path: Path):
+    assert day24.first(fixture_path / "example24.txt") == 10
+
+
+def test_second(fixture_path: Path):
+    assert day24.second(fixture_path / "example24.txt") == 2208
