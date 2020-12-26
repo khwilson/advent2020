e5e141f93dbcfee108bc68001dda7597b8cf85ab
Kevin Wilson <khwilson@gmail.com>
2020-12-26 12:28:59 -0500

    Day 25

--- advent/day25.py
+++ advent/day25.py
@@ -6,15 +6,38 @@ from typing import Union
 import numpy as np
 
 
+def disrete_logarithm(answer, base, mod):
+    """
+    Compute x where answer == pow(base, x, mod). Some notes:
+        * This is done by linearly searching, so don't actually use this
+          for real cryptographic applications
+        * This probably won't do what you think it does if base isn't a
+          primitive root modulo mod
+    """
+    log = 1
+    val = base
+    while True:
+        if answer == val:
+            return log
+        log += 1
+        val = (val * base) % mod
+
+
 def first(filename: Union[str, Path]) -> int:
     """
-    Part 1
+    Our inputs are 7^x and 7^y modulo 20201227. With these inputs,
+    compute 7^{xy}.
     """
-    return None
+    subject_number = 7
+    card_public_key, door_public_key = map(
+        int, open(filename, "rt").read().strip().split()
+    )
+    card_loop_size = disrete_logarithm(card_public_key, subject_number, 20201227)
+    return pow(door_public_key, card_loop_size, 20201227)
 
 
-def second(filename: Union[str, Path]) -> int:
+def second(filename: Union[str, Path]) -> str:
     """
     Part 2
     """
-    return None
+    return "Merry Christmas!"
--- tests/test_day25.py
+++ tests/test_day25.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
-from advent import day05
+from advent import day25
 
-# def test_first(fixture_path: Path):
-#     assert day05.first(fixture_path / "example05.txt") == 514579
+
+def test_discrete_logarith():
+    assert day25.disrete_logarithm((3 ** 13) % 17, 3, 17) == 13
+
+
+def test_first(fixture_path: Path):
+    assert day25.first(fixture_path / "example25.txt") == 14897079
 
 
 # def test_second(fixture_path: Path):
